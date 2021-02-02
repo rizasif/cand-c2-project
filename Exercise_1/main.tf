497e2dcbb006b8c588926d8df07ceb37da1b638f
rizasif <rizasif@gmail.com>
2021-02-02 20:56:00 +0200

    prepictures

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,41 @@
 # TODO: Designate a cloud provider, region, and credentials
 
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region = "us-east-1"
+}
+
+data "aws_vpc" "selected" {
+  id = "vpc-5e815223"
+}
+
+data "aws_subnet" "selected" {
+  availability_zone = "us-east-1d"
+  id = "subnet-9c1b99bd"
+}
+
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 
+resource "aws_instance" "UdacityT2" {
+  count = "4"
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.selected.id
+  tags = {
+    name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+
+resource "aws_instance" "UdacityM4" {
+  count = "2"
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "m4.large"
+  subnet_id = data.aws_subnet.selected.id
+  tags = {
+    name = "Udacity M4"
+  }
+}
---
+++ Exercise_2/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region = "us-east-1"
+}
+
+variable "function_name" {
+  default = "lambda_handler"
+}
+
+variable "handler" {
+  default = "lambda.lambda_handler"
+}
+
+variable "runtime" {
+  default = "python3.6"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  role             = "${aws_iam_role.lambda_exec_role.arn}"
+  handler          = "${var.handler}"
+  runtime          = "${var.runtime}"
+  filename         = "lambda.zip"
+  function_name    = "${var.function_name}"
+  source_code_hash = "${filebase64("lambda.zip")}"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name        = "lambda_exec"
+  path        = "/"
+  description = "Allows Lambda Function to call AWS services on your behalf."
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
